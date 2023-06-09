0daf86a1cbcc757ba212e0288f08c4d52f18ba38
ShreyaDalwadi <126682947+ShreyaDalwadi@users.noreply.github.com>
2023-06-09 19:16:21 +0530

    .

--- Assets/Script/Score.cs
+++ Assets/Script/Score.cs
@@ -9,14 +9,7 @@ public class Score : MonoBehaviour
     public int score = 0;
   
 
-    public void OnTriggerEnter2D(Collider2D collision)
-    {
-
-        score = score + 1;
-        ScoreManager.inst.scoretxt.text = score.ToString();
-        Debug.Log("scoree" + score);
-        Destroy(gameObject);
-    }
+   
     private void Awake()
     {
       
